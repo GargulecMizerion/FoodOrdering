c4d622eb92d5a2635ef61ef3e0319f010c5a82ff
GargulecMizerion <xmaszekx@gmail.com>
2025-03-06 23:00:37 +0100

    Made some model classes and configured db connection

---
+++ backend/src/main/java/com/kayman/backend/dto/RestaurantDto.java
@@ -0,0 +1,21 @@
+package com.kayman.backend.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Embeddable
+public class RestaurantDto {
+
+    private String title;
+
+    @Column(length = 1000) // URL moze byc dlugi
+    public List<String> images;
+
+    private String description;
+
+    private Long id;
+}
---
+++ backend/src/main/java/com/kayman/backend/model/Order.java
@@ -0,0 +1,22 @@
+package com.kayman.backend.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+
+}
