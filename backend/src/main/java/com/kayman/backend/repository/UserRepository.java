3dc5d1955c7467165a2b735dcd8def145e6798ff
GargulecMizerion <xmaszekx@gmail.com>
2025-03-07 22:24:28 +0100

    Started spring security configuration

---
+++ backend/src/main/java/com/kayman/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kayman.backend.repository;
+
+import com.kayman.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String email);
+}
