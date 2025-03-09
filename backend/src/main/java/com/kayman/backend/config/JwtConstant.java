a0a37f1398da47d801806d45b53730aa667f9664
GargulecMizerion <xmaszekx@gmail.com>
2025-03-09 19:03:41 +0100

    Still configuring Spring Security

---
+++ backend/src/main/java/com/kayman/backend/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.kayman.backend.config;
+
+public class JwtConstant  {
+    public static final String SECRET_KEY = "dsfg34tr4rgsfdtg34t4re";
+    public static  final  String JWT_HEADER = "Authorization";
+}
